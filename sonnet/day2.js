75779330b618ec42222362af76089c98bd3ccd72
drworm75 <dwgames@gmail.com>
2017-01-24 19:22:45 -0600

    finished switch

---
+++ sonnet/day2.js
@@ -0,0 +1,25 @@
+// switch statement
+
+var day = "Friday";
+
+switch(day){
+	case "Monday":
+		console.log("Boooooo!");
+		break;
+	case "Friday":
+		console.log("Party!");
+		break;	
+	default:
+		console.log("not Monday");	
+}
+
+
+
+if (day === "Monday"){
+	console.log("Boooooo!")
+} else if (day === "Friday"){
+	console.log("Party!");
+} else {
+	console.log("Party!");	
+}
+
