4a2627e2d2642f56e55d1f022eac854658049ea0
drworm75 <dwgames@gmail.com>
2017-01-24 18:52:44 -0600

    reviewed js as a class

--- sonnet/sonnet.js
+++ sonnet/sonnet.js
@@ -3,9 +3,8 @@
 var element = document.getElementById("sonnet");
 var theBard = element.innerHTML;
 
-console.log("element.innerHTML pulls the following text from the sonnet id: \n", theBard);
 // Find and output the starting position of the word "orphans"
-var orphansStart = theBard.search("orphans");
+var orphansStart = theBard.indexOf("orphans");
 console.log("In the sonnet, the word orphans starts at index: \n", orphansStart);
 // Output the number of characters in the sonnet
 console.log("The sonnet has ", theBard.length, "total characters");
@@ -13,7 +12,7 @@ console.log("The sonnet has ", theBard.length, "total characters");
 var xmasBard = theBard.replace("winter", "yuletide");
 console.log("Using the function replace, we can exchange the word winter with yuletide: \n", xmasBard);
 // Replace all occurances of the string "the" with "a large"
-var largeBard = xmasBard.replace(/the/g, "a large");
+var largeBard = xmasBard.replace(/the\s/g, "a large ");
 console.log("Using the function replace and a global modififer (/the/g), we can exchange every instance of the word \"the\" with \"a large\": \n", largeBard);
 // Set the content of the sonnet div with the new string
 element.innerHTML= largeBard;
