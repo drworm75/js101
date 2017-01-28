c0a7158c3fb583a8f6d8622daae333628185e724
drworm75 <dwgames@gmail.com>
2017-01-28 13:22:39 -0600

    completed day 3 classwork

--- sonnet/day3.js
+++ sonnet/day3.js
@@ -1,14 +1,47 @@
-var myCat = {
-	tail: "long",
-	age: 29,
-	color: "blue",
-	friends: ["zoe", "callan", "lauren"]
+// var myCat = {
+// 	tail: "long",
+// 	age: 29,
+// 	color: "blue",
+// 	friends: ["zoe", "callan", "lauren"]
+// }
+
+// console.log("Age of cat: ", myCat.age);
+// console.log("Color of cat: ", myCat["color"]);
+
+// for (var myKey in myCat) {
+// 	console.log("myKey", myKey);
+// 	console.log("values", myCat[myKey]);
+// }
+
+
+function nuggetMaker(animal) {
+	var myNugget = animal + " nugget"
+	return myNugget;
 }
 
-console.log("Age of cat: ", myCat.age);
-console.log("Color of cat: ", myCat["color"]);
-
-for (var myKey in myCat) {
-	console.log("myKey", myKey);
-	console.log("values", myCat[myKey]);
+function shackMaker(divLocation, typeOfNugget) {
+	divLocation.innerHTML += typeOfNugget + "<br>";
 }
+
+var chickenNuggets = nuggetMaker("chicken");
+shackMaker(availElement, chickenNuggets);
+
+var lobsterNuggets = nuggetMaker("lobster");
+shackMaker(availElement, lobsterNuggets);
+
+var chipmuckNuggets = nuggetMaker("chipmuck");
+shackMaker(availElement, chipmuckNuggets);
+
+
+
+
+var colors = ["red", "blue", "gold"]
+
+function myLooper(myArray) {
+	for (var i = 0; i < myArray.length; i++) {
+		console.log(myArray[i]);
+	}
+}
+
+myLooper(colors);
+
